c5964110cd5cfecc450a35d54bc3159919142eae
Hannah Fischer <hannahkath04@gmail.com>
2025-11-04 15:53:34 -0500

    Added some color to the homepage and added a new logo.

--- travel-planner/components/HomePage.tsx
+++ travel-planner/components/HomePage.tsx
@@ -3,9 +3,61 @@ import { Suspense } from "react";
 
 const HomePage = () => {
   return (
-    <Stack w={"100%"}>
+    <Stack  w={"100%"}>
+      <Paper
+      style={{
+        backgroundColor: '#f9f9f9ff',
+        color: 'black',
+        textAlign: "center",
+      }}
+      >
+         Travel Planner is a new way for travelers to keep
+          track of trips, and an itinerary and travel info by trip
+      </Paper>
       <AspectRatio ratio={16 / 9}>
-        <Paper withBorder>Blah blah</Paper>
+        <Paper 
+        style={{
+          backgroundColor: '#f9f9f9ff',
+          color: 'black',
+          textAlign: "center",
+        }}
+        withBorder
+        >
+          Trips
+          <Paper>
+            info about trips along with images maybe?
+          </Paper>
+        </Paper>
+      </AspectRatio>
+      <AspectRatio ratio={16 / 9}>
+        <Paper 
+        style={{
+          backgroundColor: '#f9f9f9ff',
+          color: 'black',
+          textAlign: "center",
+        }}
+        withBorder
+        >
+          Itinerary
+          <Paper>
+            info about Itinerary along with images maybe?
+          </Paper>
+        </Paper>
+      </AspectRatio>
+      <AspectRatio ratio={16 / 9}>
+        <Paper 
+        style={{
+          backgroundColor: '#f9f9f9ff',
+          color: 'black',
+          textAlign: "center",
+        }}
+        withBorder
+        >
+          Travel
+          <Paper>
+            info about Travel along with images maybe?
+          </Paper>
+        </Paper>
       </AspectRatio>
       <Group grow p="0" h={"100%"}>
         <Suspense fallback={<Skeleton w={"100%"} h={300} />}></Suspense>
