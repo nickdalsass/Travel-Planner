22a7e296a899c7f6bc10403c139fb8496926959d
Hannah Fischer <hannahkath04@gmail.com>
2025-11-21 17:47:53 -0500

    changed the homepage so that little blurb only appears when not signed in

--- travel-planner/components/static-pages/HomePage.tsx
+++ travel-planner/components/static-pages/HomePage.tsx
@@ -1,8 +1,30 @@
+"use client";
+
 import { Group, Paper, Stack, Container, Title } from '@mantine/core';
 import ImageDisplay from '../ImageDisplay';
 import CreateTripButton from '../CreateTripButton';
+import {supabase} from "@/lib/supabase/client";
+import {useEffect, useState} from 'react';
+import { User } from "@supabase/supabase-js";
 
 export default function HomePage() {
+
+  //ensuring "Sign in to start you adventure" only shows up when not logged in
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUser = async ()=>{
+      const{
+        data: {user},
+      } = await supabase.auth.getUser();
+      setUser(user);
+      setLoading(false);
+    };
+    getUser();
+  }, []
+  );
+
   return (
     <Container size="lg" p={0} style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
       <Paper
@@ -17,11 +39,15 @@ export default function HomePage() {
           <CreateTripButton />
         </Group>
       </Paper>
+    
+        <Stack>
+          {!loading && !user && (
+          <Title className='typewriter'>Sign In To Start Your Adventure...</Title>
+          )}
+          <ImageDisplay />
+        </Stack>
 
-      <Stack>
-        <Title className='typewriter'>Sign In To Start Your Adventure...</Title>
-        <ImageDisplay />
-      </Stack>
     </Container>
   );
 }
+
